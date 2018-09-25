ba2844312a55b7ce06fc2a995261be2993369621
Phi Xuan Hoan <phixuanhoan@Phis-MacBook-Pro.local>
2018-09-25 12:50:06 +0700

    update commandline

---
+++ dic-commandline/src/dic/commandline/Dictionary.java
@@ -0,0 +1,8 @@
+package dic.commandline;
+
+import java.util.ArrayList;
+
+public class Dictionary {
+
+    static ArrayList<Word> listWord = new ArrayList<>();
+}
---
+++ dic-commandline/src/dic/commandline/DictionaryCommandline.java
@@ -0,0 +1,56 @@
+package dic.commandline;
+
+import java.util.*;
+
+public class DictionaryCommandline {
+
+    public void dictionaryBasic() {
+
+    }
+
+    public void dictionaryAdvanced() {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DictionaryManagement DicMana = new DictionaryManagement();
+        DicMana.insertFromFile();
+
+        int option;
+        do {
+            System.out.println("---------------------------------");
+            System.out.println("-------Từ điển Anh - Việt--------");
+            System.out.println("----------Nhập lựa chọn----------");
+            System.out.println("1. Tra từ");
+            System.out.println("2. Thêm từ");
+            System.out.println("3. Sửa từ");
+            System.out.println("4. Xóa từ");
+            System.out.println("5. In tất cả các từ trong từ điển");
+            System.out.println("6. Kết thúc");
+            System.out.println("---------------------------------");
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    DicMana.dictionarySearcher();
+                    break;
+                case 2:
+                    DicMana.insertFromCommandline();
+                    break;
+                case 3:
+                    DicMana.editWordInDic();
+                    break;
+                case 4:
+                    DicMana.deleteWordInDic();
+                    break;
+                case 5:
+                    DicMana.showAllWords();
+                    break;
+            }
+            DicMana.dictionaryExportToFile();
+
+        } while (option >= 1 && option <= 5);
+    }
+
+}
