b85a1ae70284745683e866c8351659dd24254111
Nguyễn Trường Giang <38888083+nguyengiangk62uet@users.noreply.github.com>
2018-10-12 21:43:05 +0700

    update

---
+++ dictionary-commandline/src/dic/commandline/Dictionary.java
@@ -0,0 +1,10 @@
+package dic.commandline;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author GotTheRuns
+ */
+public class Dictionary {
+    public static ArrayList<Word> listWord = new ArrayList<>();
+}
