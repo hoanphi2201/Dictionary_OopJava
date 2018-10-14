cfaa4bc3e9cc94b614f5be8df6ec67cb8e599f09
Nguyen Giang <truonggiangpctn@gmail.com>
2018-10-14 16:45:27 +0700

    12/10/2018

---
+++ Dictionary-Commandline/src/dictionary/commandline/Dictionary.java
@@ -0,0 +1,10 @@
+package dictionary.commandline;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author GotTheRuns
+ */
+public class Dictionary {
+    public static ArrayList<Word> listWord = new ArrayList<>();
+}
